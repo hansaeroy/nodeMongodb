aa8c7156077eea1e96a3b4de19226acc43c7fdaf
hansaeroy <hyc0331@gmail.com>
2021-10-01 23:03:27 +0900

    두번째 수정

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
+// const bodyParser = require('body-parser');
+// const { User } = require ('./models/User');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hansaeroy:haha1q2w@hanseroy.mh77e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify: false
-}).then(() => console.log('MongoDB connected...')).catch(err => console.log(err))
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://hansaeroy:abcd1234@hans.mh77e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
 
+app.get("/", (요청, 응답) => {
+  응답.send("Hello World!");
+});
+
+// app.post('/register', (req, res) => {
+//   //회원가입 할때 필요한 정보들은 client에서 가져오면  데이터베이스에 넣어준다.
+//   const user = new User(req.body)
+
+//   user.save((err, userInfo) => {
+//     if (err) return res.json({success: false, err})
+//     return res.status(200).json({
+//       success:true
+//     })
+//   })
+
+// })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- models/User.js
+++ models/User.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   name: {
-      type: String,
-      maxlength: 50
+    type: String,
+    maxlength: 50,
   },
   email: {
-      type: String,
-      trim: true,
-      unique: 1
+    type: String,
+    trim: true,
+    unique: 1,
   },
   password: {
-      type: String,
-      minlength: 5
+    type: String,
+    minlength: 5,
   },
   lastname: {
-      type: String,
-      maxlength: 50
+    type: String,
+    maxlength: 50,
   },
   role: {
-      type: Number,
-      default: 0
+    type: Number,
+    default: 0,
   },
   image: String,
   token: {
-      type: String
+    type: String,
   },
   tokenExp: {
-      type: Number
-  }
-})
+    type: Number,
+  },
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = { User }
+module.exports = { User };
